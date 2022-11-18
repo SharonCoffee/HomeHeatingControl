3fd367903f16e7835b9d054933f19b631fd0dbb4
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-18 20:35:41 +0000

    feat:  added in JavaDocs explanations

--- src/main/java/cs3500/R1_WiFiConnect.java
+++ src/main/java/cs3500/R1_WiFiConnect.java
@@ -1,5 +1,10 @@
 package cs3500;
 
+/**
+ * Pre-condition:
+ * user has entered invalid password to connect heating control system to the home wi-fi.
+ * @return returns string message "Invalid Password Entered."
+ */
 public class R1_WiFiConnect {
     public String getMessage() {
         return "Invalid Password entered.";

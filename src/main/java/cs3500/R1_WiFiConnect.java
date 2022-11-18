0410c7382fba6353504b6478cf4e0360dbd7e501
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-18 20:28:07 +0000

    test: co-wrote tests for R1_WiFiConnectTest.java with Elaine O'Sullivan feat: added in a getMessage() method to perform invalid password test

--- src/main/java/cs3500/R1_WiFiConnect.java
+++ src/main/java/cs3500/R1_WiFiConnect.java
@@ -1,4 +1,7 @@
 package cs3500;
 
 public class R1_WiFiConnect {
+    public String getMessage() {
+        return "Invalid Password entered.";
+    }
 }
--- src/test/java/cs3500/R1_WiFiConnectTest.java
+++ src/test/java/cs3500/R1_WiFiConnectTest.java
@@ -1,12 +1,41 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class R1_WiFiConnectTest {
 
+    R1_WiFiConnect WiFiConnect;
+
     @BeforeEach
     void setUp() {
+        WiFiConnect = new R1_WiFiConnect();
     }
+
+    @Test
+    @DisplayName("User enters password to wi-fi connection and connection is made")
+    void enterPassword() {
+        String ENTERED_PASSWORD = ("ThisIsMyPassword");
+        // user entered below string
+        String actualValue = "ThisIsMyPassword";
+        assertEquals(ENTERED_PASSWORD, actualValue);
+    }
+
+    @Test
+    @DisplayName("User enters incorrect password to wi-fi connection and is not connected")
+    void enterInvalidPassword() {
+        String ENTERED_PASSWORD = ("ThisIsNotMyPassword");
+        // user entered below string
+        String actualValue = "ThisIsMyPassword";
+        if (ENTERED_PASSWORD != actualValue) {
+            WiFiConnect.getMessage();
+        }
+        assertTrue(WiFiConnect.getMessage() == "Invalid Password entered.");
+    }
+
+
+
 }

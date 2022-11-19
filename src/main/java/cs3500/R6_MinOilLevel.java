78e11fbcc405a2be189e9204f64ea588883842a7
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-19 12:57:29 +0000

    feat:  created several tests to test minimum oil level in conjunction with Tomasz Trela

--- src/main/java/cs3500/R6_MinOilLevel.java
+++ src/main/java/cs3500/R6_MinOilLevel.java
@@ -1,4 +1,46 @@
 package cs3500;
 
+import java.util.ArrayList;
+
 public class R6_MinOilLevel {
+
+    public ArrayList<Integer> oilLevel;
+    public final int cMIN_OIL_LEVEL = 0;
+    public final int cMAX_OIL_LEVEL = 0;
+
+    public R6_MinOilLevel() {
+        oilLevel = new ArrayList<>();
+    }
+    public int size() {
+        return oilLevel.size();
+    }
+
+    public void push(int oilLevelValue) {
+        oilLevel.add(oilLevelValue);
+    }
+
+    public String message(String textMessage) {
+        textMessage = "Minimum oil level has been reached.";
+        return textMessage;
+    }
+
+    public void addOilLevels(int minOilLevelValue, int maxOilLevelValue) {
+        if (minOilLevelValue < cMIN_OIL_LEVEL) {
+            oilLevelTooLow("Oil level selected below minimum oil level value.");
+        } else if (maxOilLevelValue > cMAX_OIL_LEVEL) {
+            oilLevelTooHigh("Oil level selected above maximum oil level value.");
+        } else {
+            for (int i = minOilLevelValue; i <= maxOilLevelValue; i++) {
+                oilLevel.add(i);
+            }
+        }
+    }
+
+    private String oilLevelTooHigh(String maximumMessage) {
+        return maximumMessage;
+    }
+
+    private String oilLevelTooLow(String minimumMessage) {
+        return minimumMessage;
+    }
 }
--- src/test/java/cs3500/R6_MinOilLevelTest.java
+++ src/test/java/cs3500/R6_MinOilLevelTest.java
@@ -1,12 +1,67 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class R6_MinOilLevelTest {
 
+    R6_MinOilLevel minOilLevel;
     @BeforeEach
     void setUp() {
+        minOilLevel = new R6_MinOilLevel();
     }
+
+    @Test
+    @DisplayName("Minimum oil level array is empty")
+    void minimumOilLevelArrayIsEmpty() {
+        assertTrue(minOilLevel.size() == 0);
+    }
+
+    @Test
+    @DisplayName("Set minimum oil level")
+    void setMinimumOilLevel() {
+        minOilLevel.push(200);
+        assertEquals(1, minOilLevel.size());
+    }
+
+    @Test
+    @DisplayName("Send message that minimum oil level has been reached")
+    void messageReachedMinimumOilLevel() {
+        final String EXPECTED_MESSAGE = "Minimum oil level has been reached.";
+        String actualMessage = minOilLevel.message("Minimum oil level has been reached.");
+        assertEquals(EXPECTED_MESSAGE, actualMessage);
+
+    }
+
+    @Test
+    @DisplayName("Add minimum oil levels to ArrayList")
+    //Assumption:  Oil tank capacity is 1000L
+    //Minimum oil levels can be set between 100L and 500L
+    void addMinimumOilLevels() {
+        int EXPECTED_MIN = 100;
+        int EXPECTED_MAX = 500;
+        minOilLevel.addOilLevels(EXPECTED_MIN, EXPECTED_MAX);
+        int actualMinimum = 100;
+        int actualMaximum = 500;
+        assertEquals(EXPECTED_MIN, actualMinimum);
+        assertEquals(EXPECTED_MAX, actualMaximum);
+
+    }
+
+    @Test
+    @DisplayName("Minimum oil levels added to ArrayList is not within bounds")
+    void addMinimumOilLevelsOutsideBounds() {
+        int EXPECTED_MIN = 100;
+        int EXPECTED_MAX = 500;
+        minOilLevel.addOilLevels(EXPECTED_MIN, EXPECTED_MAX);
+        int actualMinimum = 0;
+        int actualMaximum = 600;
+        assertNotEquals(EXPECTED_MIN, actualMinimum);
+        assertNotEquals(EXPECTED_MAX, actualMaximum);
+
+    }
+
 }

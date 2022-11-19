14de97ba5afb885de3b978f1fc1ae2acad49770d
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-19 13:58:04 +0000

    feat:  created new tests in R8_AlertsTest.java with Tomasz Trella

--- src/main/java/cs3500/R8_Alerts.java
+++ src/main/java/cs3500/R8_Alerts.java
@@ -1,4 +1,5 @@
 package cs3500;
 
 public class R8_Alerts {
+    //insert code here
 }
--- src/test/java/cs3500/R8_AlertsTest.java
+++ src/test/java/cs3500/R8_AlertsTest.java
@@ -1,12 +1,31 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class R8_AlertsTest {
 
+    R8_Alerts alerts;
+
     @BeforeEach
     void setUp() {
+        alerts = new R8_Alerts();
+    }
+
+    @Test
+    @DisplayName("Test if Alert is turned on")
+    void testStateIsOn() {
+        String actualValue = "ON";
+        assertEquals(R8_AlertStates.ON, R8_AlertStates.valueOf(actualValue));
+    }
+
+    @Test
+    @DisplayName("Test if Alert is turned off")
+    void testStateIsOff() {
+        String actualValue = "OFF";
+        assertEquals(R8_AlertStates.OFF, R8_AlertStates.valueOf(actualValue));
     }
 }

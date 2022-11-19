10f8d96291b92a3f853d77d8ce3c7add2a75646a
goblintoy <120425796@umail.ucc.ie>
2022-11-19 21:57:41 +0000

    Added and tested new method to set target water temperature

--- src/main/java/cs3500/R15_TargetWaterTemp.java
+++ src/main/java/cs3500/R15_TargetWaterTemp.java
@@ -1,4 +1,9 @@
 package cs3500;
 
 public class R15_TargetWaterTemp {
+    public int settargetwatertemp(int i) {
+        int targetWaterTemp = i;
+
+        return targetWaterTemp;
+    }
 }
--- src/test/java/cs3500/R15_TargetWaterTempTest.java
+++ src/test/java/cs3500/R15_TargetWaterTempTest.java
@@ -1,6 +1,8 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +10,14 @@ class R15_TargetWaterTempTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    @DisplayName("Should set targetwatertemp to 70")
+
+    void setTargetWaterTempTo70(){
+        R15_TargetWaterTemp targetWaterTemp = new R15_TargetWaterTemp();
+
+        assertEquals(70,targetWaterTemp.settargetwatertemp(70));
     }
 }

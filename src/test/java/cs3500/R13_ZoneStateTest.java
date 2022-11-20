3c7e75b1f4f940afe91974f4caf355aee6604c35
goblintoy <120425796@umail.ucc.ie>
2022-11-20 13:31:19 +0000

    Tested to see if zone turned on

--- src/test/java/cs3500/R13_ZoneStateTest.java
+++ src/test/java/cs3500/R13_ZoneStateTest.java
@@ -1,12 +1,23 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class R13_ZoneStateTest {
 
+    R13_ZoneState zonestate;
     @BeforeEach
-    void setUp() {
-    }
+    void setUp() { zonestate = new R13_ZoneState();}
+        @Test
+        @DisplayName("Test if zone is on")
+        void zonestateIsOn() {
+            String actualValue = "ON";
+            assertEquals(R13_ZoneStateEnum.ON, R13_ZoneStateEnum.valueOf(actualValue));
+        }
+
+
+
 }

8040202540f139a9edc90e9b4dc5bb9bc016a530
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-21 11:13:44 +0000

    feat:  added in test cases for activity logs in R10_ActivityLogTest.java with corresponding methods in R10_ActivityLog.java

--- src/test/java/cs3500/R10_ActivityLogTest.java
+++ src/test/java/cs3500/R10_ActivityLogTest.java
@@ -1,12 +1,108 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class R10_ActivityLogTest {
 
+    R10_ActivityLog activityLog;
+
     @BeforeEach
     void setUp() {
+        activityLog = new R10_ActivityLog();
     }
+
+    @Test
+    @DisplayName("Target temperature change activity is logged")
+    void testTargetTemperatureChangeActivity() {
+        int NEW_TARGET_ROOM_TEMP = 21;
+        assertTrue(activityLog.TargetRoomTempChangeIsSuccessfullyLogged(NEW_TARGET_ROOM_TEMP));
+    }
+
+    @Test
+    @DisplayName("Boost state selected")
+    void testBoostStateSelected() {
+        String BOOST_ON_STATE = "ON";
+        assertTrue(activityLog.BoostStateSelectedIsSuccessfullyLogged(BOOST_ON_STATE));
+    }
+
+    @Test
+    @DisplayName("Boost state cancelled")
+    void testBoostStateCancelled() {
+        // Boost state will change to off once boost time frame has ended
+        // Assumption - boost time frame as ended
+        String BOOST_OFF_STATE = "OFF";
+        assertTrue(activityLog.BoostStateCancelledIsSuccessfullyLogged(BOOST_OFF_STATE));
+
+    }
+
+    @Test
+    @DisplayName("Auto state has been activated")
+    void testAutoStateActivated() {
+        String AUTO_ON_STATE = "ON";
+        assertTrue(activityLog.AutoStateActivatedIsSuccessfullyLogged(AUTO_ON_STATE));
+
+    }
+
+    @Test
+    @DisplayName("Auto state has been deactivated")
+    void testAutoStateDeactivated() {
+        String AUTO_OFF_STATE = "OFF";
+        assertTrue(activityLog.AutoStateDeactivatedIsSuccessfullyLogged(AUTO_OFF_STATE));
+
+    }
+
+    @Test
+    @DisplayName("On state has been activated")
+    void testOnStateActivated() {
+        String ON_STATE = "ON";
+        assertTrue(activityLog.OnStateActivatedIsSuccessfullyLogged(ON_STATE));
+    }
+
+    @Test
+    @DisplayName("Off state has been activated")
+    void testOffStateActivated() {
+        String OFF_STATE = "OFF";
+        assertTrue(activityLog.OffStateActivatedIsSuccessfullyLogged(OFF_STATE));
+    }
+
+    @Test
+    @DisplayName("Auto Time interval schedule has been changed.")
+    void testAutoTimeIntervalScheduleHasChanged() {
+        int YEAR = 2022;
+        int MONTH = 11;
+        int DAY = 20;
+        int START_HOUR = 19;
+        int START_MINUTES = 30;
+        int END_HOUR = 21;
+        int END_MINUTES = 00;
+        assertTrue(activityLog.AutoTimeIntervalScheduleChangeIsSuccessfullyLogged(YEAR, MONTH, DAY, START_HOUR, START_MINUTES, END_HOUR, END_MINUTES));
+
+    }
+
+    // Insert i)	Holiday time interval schedule on
+    // Insert j)	Holiday time interval schedule off
+
+    @Test
+    @DisplayName("Frost Protection has been turned on")
+    void testFrostProtectionTurnedOn() {
+        String FROST_PROTECTION_ON = "ON";
+        assertTrue(activityLog.frostProtectionTurnedOnIsSuccessfullyLogged(FROST_PROTECTION_ON));
+    }
+
+    @Test
+    @DisplayName("Frost Protection has been turned off")
+    void testFrostProtectionTurnedOff() {
+        String FROST_PROTECTION_OFF = "OFF";
+        assertTrue(activityLog.frostProtectionTurnedOffIsSuccessfullyLogged(FROST_PROTECTION_OFF));
+    }
+
+
+
 }

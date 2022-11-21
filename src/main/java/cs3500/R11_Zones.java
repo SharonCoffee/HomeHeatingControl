5c240b40731793f1348184c2296ff3f8d7b1b192
goblintoy <120425796@umail.ucc.ie>
2022-11-21 13:48:55 +0000

    Added new method to get a zone's attributes

--- src/main/java/cs3500/R11_Zones.java
+++ src/main/java/cs3500/R11_Zones.java
@@ -1,4 +1,16 @@
 package cs3500;
-
+;
 public class R11_Zones {
+
+    R13_ZoneState zoneState = new R13_ZoneState();
+
+    R15_TargetWaterTemp targetWaterTemp = new R15_TargetWaterTemp();
+
+    R14_TargetRoomTemp targetRoomTemp = new R14_TargetRoomTemp();
+
+    Object[] aArrayOfZoneAttributes = new Object[6];
+
+    public Object[] getZoneAttributes() {
+        return aArrayOfZoneAttributes;
+    }
 }
--- src/test/java/cs3500/R11_ZonesTest.java
+++ src/test/java/cs3500/R11_ZonesTest.java
@@ -1,12 +1,41 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class R11_ZonesTest {
 
+    R11_Zones zones;
+
+    R13_ZoneState zoneState;
+
+    R15_TargetWaterTemp targetWaterTemp;
+
+    R14_TargetRoomTemp targetRoomTemp;
+
     @BeforeEach
-    void setUp() {
+    void setUp()  {
+       zones = new R11_Zones();
+
+       zoneState = new R13_ZoneState();
+
+       targetWaterTemp = new R15_TargetWaterTemp();
+
+       targetRoomTemp = new R14_TargetRoomTemp();
+    }
+
+    @Test
+    @DisplayName("Test to see can get aArrayOfZoneAttributes")
+    void zoneAttributesAreGet() {
+        Object[] testArray = new Object[6];
+
+        assertArrayEquals(testArray, zones.getZoneAttributes());
     }
 }

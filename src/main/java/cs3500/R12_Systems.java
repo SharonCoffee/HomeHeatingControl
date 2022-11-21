d0f9a703ae09a9314f8060217ab8e5713f2eed02
goblintoy <120425796@umail.ucc.ie>
2022-11-21 21:24:04 +0000

    Added method to get name of systems

--- src/main/java/cs3500/R12_Systems.java
+++ src/main/java/cs3500/R12_Systems.java
@@ -1,4 +1,10 @@
 package cs3500;
 
 public class R12_Systems {
+
+    String systemName = "Unknown";
+
+    public String getName() {
+        return systemName;
+    }
 }
--- src/test/java/cs3500/R12_SystemsTest.java
+++ src/test/java/cs3500/R12_SystemsTest.java
@@ -1,12 +1,26 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class R12_SystemsTest {
 
+    R12_Systems systems;
+
     @BeforeEach
     void setUp() {
+
+        systems = new R12_Systems();
+
+    }
+    @Test
+    @DisplayName("Test if can get home name")
+    void getSystemName() {
+        String testName = "Unknown";
+
+        assertEquals(testName, systems.getName());
     }
 }

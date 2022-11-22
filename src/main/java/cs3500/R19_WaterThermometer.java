de2ce4835b04fd1ea04354bb33a16e1b33894edb
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-22 15:22:46 +0000

    feat:  add tests to check if heater turns on if current water temperature is lower than target water temperature and heater turns off if current water temperature is higher than target water temperature.

--- src/main/java/cs3500/R19_WaterThermometer.java
+++ src/main/java/cs3500/R19_WaterThermometer.java
@@ -1,4 +1,23 @@
 package cs3500;
 
 public class R19_WaterThermometer {
+
+    private int currentWaterTemperatureValue;
+    private int targetWaterTemperatureValue;
+
+    public void setCurrentWaterTemperature(int currentWaterTemperatureValue) {
+        this.currentWaterTemperatureValue = currentWaterTemperatureValue;
+    }
+
+    public boolean isHeating() {
+        return true;
+    }
+
+    public void setTargetWaterTemperature(int targetWaterTemperatureValue) {
+        this.targetWaterTemperatureValue = targetWaterTemperatureValue;
+    }
+
+    public boolean isNotHeating() {
+        return true;
+    }
 }
--- src/test/java/cs3500/R19_WaterThermometerTest.java
+++ src/test/java/cs3500/R19_WaterThermometerTest.java
@@ -1,12 +1,95 @@
 package cs3500;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class R19_WaterThermometerTest {
 
+    R19_WaterThermometer waterThermostat;
+
+    int WATER_TEMP_DEVIATION = 1;
+
+
     @BeforeEach
     void setUp() {
+
+        waterThermostat = new R19_WaterThermometer();
+     }
+
+
+
+    @Test
+    @DisplayName("Test change in current water temperature when current water temperature is below target water temperature")
+    void testChangeCurrentWaterTemperatureBelowTarget() {
+        int CURRENT_WATER_TEMPERATURE_VALUE = 35;
+        int TARGET_WATER_TEMPERATURE_VALUE = 60;
+        for (int waterTemperature = CURRENT_WATER_TEMPERATURE_VALUE; waterTemperature < TARGET_WATER_TEMPERATURE_VALUE; waterTemperature++) {
+            waterThermostat.setCurrentWaterTemperature(waterTemperature);
+            assertTrue(waterThermostat.isHeating());
+        }
+
     }
+
+    @Test
+    @DisplayName("Test change in current water temperature when current water temperature is above target water temperature")
+    void testChangeCurrentWaterTemperatureAboveTarget() {
+        int CURRENT_WATER_TEMPERATURE_VALUE = 70;
+        int TARGET_WATER_TEMPERATURE_VALUE = 60;
+        for (int waterTemperature = CURRENT_WATER_TEMPERATURE_VALUE; waterTemperature > TARGET_WATER_TEMPERATURE_VALUE; waterTemperature--) {
+            waterThermostat.setCurrentWaterTemperature(waterTemperature);
+            assertTrue(waterThermostat.isNotHeating());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Test change in current water temperature when current water temperature is below target water temperature")
+    void testChangeTargetWaterTemperatureAboveCurrent() {
+        int CURRENT_WATER_TEMPERATURE_VALUE = 45;
+        int TARGET_WATER_TEMPERATURE_VALUE = 60;
+        for (int waterTemperature = CURRENT_WATER_TEMPERATURE_VALUE; waterTemperature < TARGET_WATER_TEMPERATURE_VALUE; waterTemperature++) {
+            waterThermostat.setCurrentWaterTemperature(waterTemperature);
+            assertTrue(waterThermostat.isHeating());
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("Test change in current water temperature when current water temperature is above target water temperature")
+    void testChangeTargetWaterTemperatureBelowCurrent() {
+        int CURRENT_WATER_TEMPERATURE_VALUE = 75;
+        int TARGET_WATER_TEMPERATURE_VALUE = 60;
+        for (int waterTemperature = CURRENT_WATER_TEMPERATURE_VALUE; waterTemperature > TARGET_WATER_TEMPERATURE_VALUE; waterTemperature--) {
+            waterThermostat.setCurrentWaterTemperature(waterTemperature);
+            assertTrue(waterThermostat.isNotHeating());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Test Heating Control is On if current water temperature is lower than target water temperature")
+    void testHeatingControlIsOn() {
+        int CURRENT_WATER_TEMPERATURE_VALUE = 35;
+        int TARGET_WATER_TEMPERATURE_VALUE = 80;
+        waterThermostat.setCurrentWaterTemperature(CURRENT_WATER_TEMPERATURE_VALUE);
+        waterThermostat.setTargetWaterTemperature(TARGET_WATER_TEMPERATURE_VALUE);
+        assertTrue(waterThermostat.isHeating());
+    }
+
+    @Test
+    @DisplayName("Test Heating Control is Off if current water temperature is higher than target water temperature")
+    void testHeatingControlIsOff() {
+        int CURRENT_WATER_TEMPERATURE_VALUE = 80;
+        int TARGET_WATER_TEMPERATURE_VALUE = 70;
+        waterThermostat.setCurrentWaterTemperature(CURRENT_WATER_TEMPERATURE_VALUE);
+        waterThermostat.setTargetWaterTemperature(TARGET_WATER_TEMPERATURE_VALUE);
+        assertTrue(waterThermostat.isNotHeating());
+    }
+
+    // test for temperature deviation
+
 }

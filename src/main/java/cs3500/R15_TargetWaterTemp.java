782bdc5d50b6fd7c17d872e5ca66c71f501e4ee6
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-22 16:13:34 +0000

    feat:  removed tests and code in conflict with Kieran's branch.

--- src/main/java/cs3500/R15_TargetWaterTemp.java
+++ src/main/java/cs3500/R15_TargetWaterTemp.java
@@ -2,29 +2,5 @@ package cs3500;
 
 public class R15_TargetWaterTemp {
 
-    int targetWaterTemp;
-    int targetWaterTempInBounds;
-
-    int cMIN_WATER_TEMP = 55;
-    int cMAX_WATER_TEMP = 80;
-    int WATER_TEMP_DEVIATION = 1;
-
-
-    public void setTargetWaterTempValue(int targetWaterTemp) {
-        this.targetWaterTemp = targetWaterTemp;
-
-    }
-
-    public int getTargetWaterTempValue() {
-        return targetWaterTemp;
-    }
-
-    public boolean setTargetWaterTempValueWithinBounds(int targetWaterTempInBounds, int cMIN_WATER_TEMP, int cMAX_WATER_TEMP) {
-        return targetWaterTempInBounds >= cMIN_WATER_TEMP && targetWaterTempInBounds <= cMAX_WATER_TEMP;
-    }
-
-    public int getTargetWaterTempValueWithinBounds() {
-        return targetWaterTempInBounds;
-    }
 
 }

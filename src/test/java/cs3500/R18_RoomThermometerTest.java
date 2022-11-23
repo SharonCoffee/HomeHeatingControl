dcfb09eaa2027a150cf302d4ab1050e4e9a27e69
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-23 13:41:39 +0000

    feat: added two tests to test if heating system turns on if current room temperature drops one degree below target room temperature and turns off if current room temperature rises one degree above target room temperature.

--- src/test/java/cs3500/R18_RoomThermometerTest.java
+++ src/test/java/cs3500/R18_RoomThermometerTest.java
@@ -17,7 +17,7 @@ class R18_RoomThermometerTest {
     }
 
     @Test
-    @DisplayName("Test change in current room temperature when current room temperature is below target room temperature")
+    @DisplayName("Test if current room temperature changes detected when current room temperature is below target room temperature")
     void testChangeCurrentRoomTemperatureBelowTarget() {
         int CURRENT_ROOM_TEMPERATURE_VALUE = 15;
         int TARGET_ROOM_TEMPERATURE_VALUE = 21;
@@ -29,7 +29,7 @@ class R18_RoomThermometerTest {
     }
 
     @Test
-    @DisplayName("Test change in current room temperature when current room temperature is above target room temperature")
+    @DisplayName("Test if current room temperature changes detected when current room temperature is above target room temperature")
     void testChangeCurrentRoomTemperatureAboveTarget() {
         int CURRENT_ROOM_TEMPERATURE_VALUE = 23;
         int TARGET_ROOM_TEMPERATURE_VALUE = 21;
@@ -41,7 +41,7 @@ class R18_RoomThermometerTest {
     }
 
     @Test
-    @DisplayName("Test change in current room temperature when current room temperature is below target room temperature")
+    @DisplayName("Test change in target room temperature when target room temperature is above current room temperature")
     void testChangeTargetRoomTemperatureAboveCurrent() {
         int CURRENT_ROOM_TEMPERATURE_VALUE = 15;
         int TARGET_ROOM_TEMPERATURE_VALUE = 20;
@@ -53,7 +53,7 @@ class R18_RoomThermometerTest {
     }
 
     @Test
-    @DisplayName("Test change in current room temperature when current room temperature is above target room temperature")
+    @DisplayName("Test change in target room temperature when target room temperature is below current room temperature")
     void testChangeTargetRoomTemperatureBelowCurrent() {
         int CURRENT_ROOM_TEMPERATURE_VALUE = 23;
         int TARGET_ROOM_TEMPERATURE_VALUE = 20;
@@ -84,6 +84,26 @@ class R18_RoomThermometerTest {
         assertTrue(roomThermostat.isNotHeating());
     }
 
-    // test for temperature deviation
+    @Test
+    @DisplayName("Test Heating Control is On if current room temperature is lower than target room temperature by one degree")
+    void testHeatingControlIsOnIfCurrentRoomTemperatureIsLower() {
+        int CURRENT_ROOM_TEMPERATURE_VALUE = 20;
+        int TARGET_ROOM_TEMPERATURE_VALUE = 20;
+        int newCurrentRoomTemperature = CURRENT_ROOM_TEMPERATURE_VALUE - ROOM_TEMP_DEVIATION;
+        roomThermostat.setCurrentRoomTemperature(newCurrentRoomTemperature);
+        roomThermostat.setTargetRoomTemperature(TARGET_ROOM_TEMPERATURE_VALUE);
+        assertTrue(roomThermostat.isHeating());
+    }
+
+    @Test
+    @DisplayName("Test Heating Control is Off if current room temperature is higher than target room temperature by one degree")
+    void testHeatingControlIsOffIfCurrentRoomTemperatureIsHigher() {
+        int CURRENT_ROOM_TEMPERATURE_VALUE = 20;
+        int TARGET_ROOM_TEMPERATURE_VALUE = 20;
+        int newCurrentRoomTemperature = CURRENT_ROOM_TEMPERATURE_VALUE + ROOM_TEMP_DEVIATION;
+        roomThermostat.setCurrentRoomTemperature(newCurrentRoomTemperature);
+        roomThermostat.setTargetRoomTemperature(TARGET_ROOM_TEMPERATURE_VALUE);
+        assertTrue(roomThermostat.isNotHeating());
+    }
 
 }

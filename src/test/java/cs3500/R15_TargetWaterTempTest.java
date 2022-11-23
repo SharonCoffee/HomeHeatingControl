4da2fd8155794e83e148c66162b5754db5d079ce
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2022-11-23 12:36:06 +0000

    feat: added @BeforeEach test code and adjusted spacing.

--- src/test/java/cs3500/R15_TargetWaterTempTest.java
+++ src/test/java/cs3500/R15_TargetWaterTempTest.java
@@ -8,75 +8,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class R15_TargetWaterTempTest {
 
+    R15_TargetWaterTemp targetWaterTemp;
+
     @BeforeEach
     void setUp() {
+        targetWaterTemp = new R15_TargetWaterTemp();
     }
 
     @Test
     @DisplayName("Should set targetWaterTemp to 70")
-
     void setTargetWaterTempTo70(){
-        R15_TargetWaterTemp targetWaterTemp = new R15_TargetWaterTemp();
-
         assertEquals(70,targetWaterTemp.setTargetWaterTemp(70));
     }
 
     @Test
     @DisplayName("Should get targetWaterTemp which is set to 65")
-
     void getTargetWaterTempTo80(){
-        R15_TargetWaterTemp targetWaterTemp = new R15_TargetWaterTemp();
-
         targetWaterTemp.setTargetWaterTemp(65);
-
         assertEquals(65,targetWaterTemp.getTargetWaterTemp());
     }
+
     @Test
     @DisplayName("Should increase targetWaterTemp by one")
-
     void increaseTargetWaterTempBy1(){
-        R15_TargetWaterTemp targetWaterTemp = new R15_TargetWaterTemp();
-
         targetWaterTemp.setTargetWaterTemp(65);
-
         assertEquals(66, targetWaterTemp.increaseTargetWaterTemp());
-
     }
 
     @Test
     @DisplayName("Should decrease targetWaterTemp by one")
-
     void decreaseTargetWaterTempBy1(){
-        R15_TargetWaterTemp targetWaterTemp = new R15_TargetWaterTemp();
-
         targetWaterTemp.setTargetWaterTemp(65);
-
         assertEquals(64, targetWaterTemp.decreaseTargetWaterTemp());
-
     }
 
     @Test
     @DisplayName("Shouldn't decrease targetWaterTemp due to hitting the minimum water temperature ")
-
     void hittingTargetWaterTempMin(){
-        R15_TargetWaterTemp targetWaterTemp = new R15_TargetWaterTemp();
-
         targetWaterTemp.setTargetWaterTemp(55);
-
         assertEquals(55, targetWaterTemp.decreaseTargetWaterTemp());
-
     }
 
     @Test
     @DisplayName("Shouldn't increase targetWaterTemp due to hitting the maximum water temperature ")
-
     void hittingTargetWaterTempMax(){
-        R15_TargetWaterTemp targetWaterTemp = new R15_TargetWaterTemp();
-
         targetWaterTemp.setTargetWaterTemp(80);
-
         assertEquals(80, targetWaterTemp.increaseTargetWaterTemp());
-
     }
 }
 
